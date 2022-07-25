ced792a1e38812f4ea98233a0c0b5101eda5fcd6
mmervecerit <mmervecerit@gmail.com>
2022-07-25 18:39:44 +0300

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import cv2
 import numpy as np
-from datetime import datetime
 
 st.set_page_config(page_title="Little Tingz", page_icon="❤️", layout="centered", initial_sidebar_state="auto", menu_items=None)
 flag = False
