a0e4e85929a5c7f5d68825f3991fb434d87c67a9
mmervecerit <mmervecerit@gmail.com>
2022-07-25 18:33:07 +0300

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,47 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import cv2
+import numpy as np
+from datetime import datetime
 
-"""
-# Welcome to Streamlit!
+st.set_page_config(page_title="Little Tingz", page_icon="❤️", layout="centered", initial_sidebar_state="auto", menu_items=None)
+flag = False
+once, donce=True,True
+var1 = st.empty()
+var2 = st.empty()
+var4 = st.empty()
+var3 = st.empty()
+var5 = st.empty()
+var6 = st.empty()
+var7 = st.empty()
+var1.title('Hello friend!🥳')
+var2.markdown(f'<h1 style="color:#FF7F50;font-size:36px;">{"Show your QR Code to the Camera for a surprise!"}</h1>', unsafe_allow_html=True)
+img_file_buffer = var3.camera_input("")
+if img_file_buffer is not None:
+    bytes_data = img_file_buffer.getvalue()
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+    decoder = cv2.QRCodeDetector()
+    data, bbox, _ = decoder.detectAndDecode(cv2_img)
+    if data:
+        var1.empty()
+        var2.empty()
+        var3.empty()
+        if data == "monster1" and once:
+            var5.balloons()
+            var2.markdown(f'<h1 style="color:#FF7F50;font-size:36px;">{"Here is your surprise from your Little Tingz!"}</h1>', unsafe_allow_html=True)
+            var6.video("https://youtu.be/t0Q2otsqC4I")
+            once=False
+            var7.text("🔃 Refresh the page to scan a new QR 🔃 ")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+        elif data == "monster2" and donce:
+            var5.balloons()
+            var2.markdown(f'<h1 style="color:#FF7F50;font-size:36px;">{"Here is your surprise from your Little Tingz!"}</h1>', unsafe_allow_html=True)
+            var6.video("https://youtu.be/1ifwADpFSyA")
+            donce=False
+            var7.text("🔃 Refresh the page to scan a new QR 🔃 ")
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+        else: 
+            st.error('Please only show Little Tingz QRs.')
+            var7.text("🔃 Refresh the page to scan a new QR 🔃 ")
+        
+    else:
+        st.error('Please show the QR Code.')
